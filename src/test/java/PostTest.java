22a3d6fafc1d9b4544a9684665a097e30b9cd652
Vladimir <vladimir.zhevagin@yandex.ru>
2021-05-08 15:32:39 +0300

    Первый

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,25 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+    @Test
+        void shouldPost(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")  //Если соберётесь отправлять текст не на латинице
+                .body("привет") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("прив")/* --> ваша проверка здесь <-- */);
+    }
+}
